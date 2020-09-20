e6a5148c55cc207ff5153cd24898457576d6aac1
Max Yao <weng.32002@gmail.com>
2020-09-20 23:51:17 +0800

    support H/M/S format to delay

--- python_sleep.py
+++ python_sleep.py
@@ -10,7 +10,24 @@ if __name__ == "__main__":
     if len(sys.argv)==argument_count:
         delay_time = sys.argv[1]
         if len(delay_time) > 0:
-            delay_time_int = int(delay_time)
+            if 'H' in delay_time:
+                delay_time = delay_time.replace('H','h')
+            if 'h' in delay_time:
+                delay_time = delay_time.replace('h','*60M')
+            if 'M' in delay_time:
+                delay_time = delay_time.replace('M','m')
+            if 'm' in delay_time:
+                delay_time = delay_time.replace('m','*60S')
+            if 'S' in delay_time:
+                delay_time = delay_time.replace('S','s')
+            if 's' in delay_time:
+                delay_time = delay_time.replace('s','*1')
+
+            delay_time_int = 0
+            if '*' in delay_time:
+                delay_time_int = eval(delay_time)
+            else:
+                delay_time_int = int(delay_time)
             print("start to sleep %d seconds." % delay_time_int)
             time.sleep(delay_time_int)
             print("end of sleep.")
