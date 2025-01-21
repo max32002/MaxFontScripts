de9c3a077e42b72f43908e43744f2557d445b0e7
Max <max32002@gmail.com>
2025-01-21 14:02:53 +0800

    support to collect unicode image file name to txt file.

--- get_ttf_chars.py
+++ get_ttf_chars.py
@@ -4,8 +4,8 @@
 import argparse
 import platform
 import LibGlyph
-
-from os.path import join, exists, normpath, basename
+import os
+from os.path import normpath, basename
 
 def output_to_file(myfile, myfont_set):
     full_text = []
@@ -33,13 +33,8 @@ def output_to_file(myfile, myfont_set):
             #pass
     myfile.write(''.join(full_text))
 
-def get_ttf_chars_list(source_ff, unicode_field, filename_output):
-    source_unicode_set, source_dict = LibGlyph.load_files_to_set_dict(source_ff, unicode_field)
-    sorted_set=sorted(source_unicode_set)
 
-    print("charset length:", len(source_unicode_set))
-    #print("output compare result to file...")
-    print("output file: %s" % filename_output)
+def save_set_to_file(sorted_set, filename_output):
     outfile = None
     if platform.system() == 'Windows':
         outfile = open(filename_output, 'w', encoding='UTF-8')
@@ -51,24 +46,46 @@ def get_ttf_chars_list(source_ff, unicode_field, filename_output):
     outfile = None
 
 def main(args):
-    source_ff = args.input
-    if not ".sfdir" in source_ff:
-        source_ff += ".sfdir"
+    source_folder = args.input
+    filename_output = args.output
+    source_unicode_set = set()
 
-    source_name = (basename(normpath(source_ff)))
-    if source_name.endswith(".sfdir"):
-        source_name = source_name[:len(source_name)-6]
-    filename_output = "charset_%s.txt" % source_name
-    if args.output:
-        filename_output = args.output
-    print("input:", source_ff)
-    print("output:", filename_output)
+    if args.mode == "fontforge":
+        if not ".sfdir" in source_folder:
+            source_folder += ".sfdir"
 
-    # from 1 to 3.
-    #unicode_field = 2       # for Noto Sans
-    unicode_field = 2
+        # from 1 to 3.
+        #unicode_field = 2       # for Noto Sans
+        unicode_field = 2
+        
+        source_unicode_set, source_dict = LibGlyph.load_files_to_set_dict(source_folder, unicode_field)
+
+    if args.mode == "unicode_image":
+        target_folder_list = os.listdir(source_folder)
+        for filename in target_folder_list:
+            if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
+                #print("image file name", filename)
+                char_string = os.path.splitext(filename)[0]
+                if len(char_string) > 0:
+                    #print("char_string", char_string)
+                    #source_unicode_set.add(chr(int(char_string)))
+                    source_unicode_set.add(int(char_string))
     
-    get_ttf_chars_list(source_ff, unicode_field, filename_output)
+    if len(source_unicode_set) > 0:
+        source_name = (basename(normpath(source_folder)))
+        if source_name.endswith(".sfdir"):
+            source_name = source_name[:len(source_name)-6]
+        if filename_output == "output.txt":
+            filename_output = "charset_%s.txt" % source_name
+
+        sorted_set=sorted(source_unicode_set)
+        save_set_to_file(sorted_set, filename_output)
+
+        print("input:", source_folder)
+        print("output:", filename_output)
+        print("charset length:", len(sorted_set))
+    else:
+        print("source folder is empty!")
 
 def cli():
     parser = argparse.ArgumentParser(
@@ -80,8 +97,14 @@ def cli():
 
     parser.add_argument("--output",
         help=".txt file path",
+        default="output.txt", 
         type=str)
     
+    parser.add_argument("--mode",
+        help="mode of folder",
+        default="fontforge", 
+        type=str)
+
     args = parser.parse_args()
     main(args)
 
