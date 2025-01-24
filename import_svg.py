9c372c945aa08c26f1bbaeab501e4f90c44c0028
Max <max32002@gmail.com>
2025-01-24 13:00:47 +0800

    簡化字型線條

--- import_svg.py
+++ import_svg.py
@@ -6,12 +6,17 @@ from os import mkdir
 from os.path import exists, normpath, basename
 import argparse
 
-def import_svg(ff_path, out_path, svg_path, filename_pattern, filename_source, scale, simplify):
-    print("Open font:", ff_path)
-    print("Save dir:", out_path)
-    print("Svg path:", svg_path)
-    print("Filename pattern:", filename_pattern)
-    print("Filename source:", filename_source)
+def import_main(args):
+    ff_path = args.input
+    out_path = args.output
+
+    scale = False
+    if args.enable_scale:
+        scale = True
+
+    simplify = True
+    if args.disable_simplify:
+        simplify = False
 
     if len(ff_path) > 0:
         if ff_path[:1] == "/":
@@ -26,10 +31,10 @@ def import_svg(ff_path, out_path, svg_path, filename_pattern, filename_source, s
     export_as_font = False
 
     if out_path is None:
-        if ff_path.endswith(".ttf"):
+        if ff_path.endswith(".ttf") or ff_path.endswith(".woff") or ff_path.endswith(".woff2"):
             export_as_font = True
     else:
-        if out_path.endswith(".ttf"):
+        if out_path.endswith(".ttf") or out_path.endswith(".woff") or out_path.endswith(".woff2"):
             export_as_font = True
 
     if not out_path is None:
@@ -37,19 +42,56 @@ def import_svg(ff_path, out_path, svg_path, filename_pattern, filename_source, s
             if not exists(out_path):
                 mkdir(out_path)
 
-    myfont=fontforge.open(ff_path)
+    print("Open font:", ff_path)
+    print("Save dir:", out_path)
+    print("Svg path:", args.svg_path)
+    print("Filename pattern:", args.filename_pattern)
+    print("Filename source:", args.filename_source)
+
+    if exists(ff_path):
+        myfont=fontforge.open(ff_path)
+        myfont, import_char_list = import_svg(myfont, args.svg_path, args.filename_pattern, args.filename_source, scale, simplify, args.begin, args.end)
+
+        # font config.
+        if not args.comment is None:
+            myfont.comment = args.comment
+        if not args.familyname is None:
+            myfont.familyname = args.familyname
+        if not args.fontname is None:
+            myfont.fontname = args.fontname
+        if not args.fullname is None:
+            myfont.fullname = args.fullname
+        if not args.macstyle is None:
+            myfont.macstyle = args.macstyle
+        if not args.weight is None:
+            myfont.weight = args.weight
+
+        if len(import_char_list) > 0:
+            if export_as_font:
+                myfont.generate(out_path)
+            else:
+                if out_path is None:
+                    myfont.save()
+                else:
+                    myfont.save(out_path)
+        else:
+            print("Do nothing due to no changed glyph.")
+
+def import_svg(myfont, svg_path, filename_pattern, filename_source, scale, simplify, index_begin=0, index_end=99999):
+
     myfont.selection.all()
 
     skip_list = []
+    import_char_list = set()
+    fail_char_list = set()
 
-    import_counter = 0
-    import_char_list = ""
+    all_glyph_list = list(myfont.selection.byGlyphs)
+    print("Source font total glyph:", len(all_glyph_list))
     
     idx = 0
-    for glyph in myfont.selection.byGlyphs:
+    for glyph in all_glyph_list:
         idx +=1
-
-        #unicode_string = str(hex(glyph.unicode))[2:]
+        
         unicode_int = glyph.unicode
         if unicode_int in skip_list:
             continue
@@ -57,12 +99,29 @@ def import_svg(ff_path, out_path, svg_path, filename_pattern, filename_source, s
         if unicode_int <= 0:
             continue
 
-        previous_width = glyph.width
+        current_glyph_width = glyph.width
         if glyph.width <= 0:
             continue
 
-        # default use 'char'
+        if idx < index_begin:
+            continue
+
+        if idx > index_end:
+            continue
+
+        # altuni, cause glyph expand stroke more times.
+        if glyph.changed:
+            continue
+
+        # due to error:
+        # internal buffer error : Memory allocation failed : growing buffer
+        # I/O error : Memory allocation failed : growing input buffer
+
         filename_variable = chr(unicode_int)
+        if glyph.originalgid in import_char_list:
+            #print("duplicated char due to altuni:", filename_variable, "id:", glyph.originalgid)
+            continue
+
         if filename_source == 'unicode_hex':
             filename_variable = str(hex(unicode_int))[2:]
         if filename_source == 'unicode_int':
@@ -74,40 +133,64 @@ def import_svg(ff_path, out_path, svg_path, filename_pattern, filename_source, s
         #debug = True
 
         if debug:
+            print("-"*20)
+            print("glyph originalgid:", glyph.originalgid)
+            print("glyph unicode:", unicode_int)
+            print("glyph altuni:", glyph.altuni)
+            print("glyph char:", chr(unicode_int))
+
             print("svg path:", svg_filepath)
             print("filename:", filename)
             print("filename_variable:", filename_variable)
+
+
+        if not exists(svg_filepath):
+            if not glyph.altuni is None:
+                for altuni_tuple in glyph.altuni:
+                    #print("glyph altuni_tuple:", altuni_tuple)
+                    unicode_value = 0
+                    try:
+                        (unicode_value, variation_selector, reserved_field) = altuni_tuple
+                    except Exception as exc:
+                        #print("glyph altuni:", glyph.altuni)
+                        print(exc)
+                        pass
+
+                    if unicode_value > 0:
+                        #print("glyph unicode_value:", unicode_value)
+                        filename=filename_pattern % (str(unicode_value))
+                        svg_filepath = os.path.join(svg_path,filename)
+                        #print("new svg path:", svg_filepath)
+                        if exists(svg_filepath):
+                            break
 
         if exists(svg_filepath):
             if debug:
                 print("found matched svg path: %s" % (svg_filepath) )
 
-            glyph.clear()
-            glyph.importOutlines(svg_filepath, scale=scale, simplify=simplify)
-            glyph.width = previous_width
-            import_counter += 1
-            import_char_list += chr(unicode_int)
+            try:
+                glyph.clear()
+                glyph.importOutlines(svg_filepath, scale=scale, simplify=simplify)
+                glyph.width = current_glyph_width
+                
+                import_char_list.add(glyph.originalgid)
+            except Exception as exc:
+                fail_char_list.add(unicode_int)
+                print("error svg path:", svg_filepath)
+                print(exc)
+                pass
 
         if idx % 1000 == 0:
-            print("Processing (%d)export" % (idx))
+            print("Processing glyph: %d" % (idx))
 
-    formated_imported_text = import_char_list
-    if len(formated_imported_text) > 300:
-        formated_imported_text = formated_imported_text[:40] + "..." + formated_imported_text[-40:]
-    print("Source font glyph count:", idx)
-    print("Imported count:", import_counter)
+    print("Imported count:", len(import_char_list))
     #print("Imported text:", formated_imported_text)
+    if len(fail_char_list) > 0:
+        print("Failed count:", len(fail_char_list))
+        print("Failed char:", fail_char_list)
 
-    if import_counter > 0:
-        if export_as_font:
-            myfont.generate(out_path)
-        else:
-            if out_path is None:
-                myfont.save()
-            else:
-                myfont.save(out_path)
-    else:
-        print("Do nothing due to no changed glyph.")
+    return myfont, import_char_list
+
 
 def cli():
     parser = argparse.ArgumentParser(
@@ -141,8 +224,8 @@ def cli():
         default="unicode_int",
         )
 
-    parser.add_argument('--disable_scale', 
-        help='disable to scale imported images and SVGs to ascender height',
+    parser.add_argument('--enable_scale', 
+        help='enable to scale imported images and SVGs to ascender height',
         action='store_true'
         )
     parser.add_argument('--disable_simplify', 
@@ -150,21 +233,42 @@ def cli():
         action='store_true'
         )
 
+    parser.add_argument('--begin', type=int, default=0)
+    parser.add_argument('--end', type=int, default=99999)
+
+    # font config.
+    parser.add_argument("--comment",
+        help="A comment associated with the font. Can be anything.",
+        type=str)
+
+    parser.add_argument("--familyname",
+        help="PostScript font family name",
+        type=str)
+
+    parser.add_argument("--fontname",
+        help="PostScript font name",
+        type=str)
+
+    parser.add_argument("--fullname",
+        help="PostScript font name",
+        type=str)
+
+    parser.add_argument("--macstyle",
+        help="Bold (if set to 1)",
+        type=int)
+
+    parser.add_argument("--weight",
+        help="PostScript font weight string",
+        type=str)
+
+    # convert name list.
+    parser.add_argument("--namelist",
+        help="namelist string",
+        default="",
+        type=str)
+
+
     args = parser.parse_args()
-
-    project_output = args.input
-    
-    if not args.output is None:
-        project_output = args.output
-
-    scale = True
-    if args.disable_scale:
-        scale = False
-
-    simplify = True
-    if args.disable_simplify:
-        simplify = False
-
 
     pass_precheck = True
     
@@ -173,7 +277,7 @@ def cli():
         print("svg path not found: %s" % (args.svg_path))
 
     if pass_precheck:
-        import_svg(args.input, project_output, args.svg_path, args.filename_pattern, args.filename_source, scale, simplify)
+        import_main(args)
 
 if __name__ == "__main__":
     cli()
--- simplify.py
+++ simplify.py
@@ -1,59 +1,19 @@
 #!/usr/bin/env python3
 #encoding=utf-8
 
-#execute command:
-# /Applications/FontForge.app/Contents/Resources/opt/local/bin/fontforge ~/Documents/noto/convert/simplify.py --input ~/Documents/noto/abc.sfdir --output xyz.sfdir
-
-# /Applications/FontForge.app/Contents/Resources/opt/local/bin/fontforge
-
 import argparse
 import math
+from os.path import exists, normpath, basename
 
-def simplify(args):
-
-    output_ff = args.output
-    if output_ff is None:
-        output_ff = args.input
-    
-    # basic settings.
-    ff_tmp = args.input
-
-    print("Open font:", ff_tmp)
-    print("Save to:", output_ff)
-    myfont=fontforge.open(ff_tmp)
-    myfont.selection.all()
-
-    # font config.
-    if not args.comment is None:
-        myfont.comment = args.comment
-    if not args.familyname is None:
-        myfont.familyname = args.familyname
-    if not args.fontname is None:
-        myfont.fontname = args.fontname
-    if not args.fullname is None:
-        myfont.fullname = args.fullname
-    if not args.macstyle is None:
-        myfont.macstyle = args.macstyle
-
+def simplify_glyph(myfont, args):
+    all_glyph_list = list(myfont.selection.byGlyphs)
+    print("Source font total glyph:", len(all_glyph_list))
 
     skip_list = []
-
     idx = 0
-
-    for glyph in myfont.selection.byGlyphs:
-        idx +=1
-        #unicode_string = str(hex(glyph.unicode))[2:]
-        unicode_int = glyph.unicode
-        if unicode_int in skip_list:
-            continue
-
-        if unicode_int <= 0:
-            continue
-
     convert_count = 0
-    for glyph in myfont.selection.byGlyphs:
+    for glyph in all_glyph_list:
         idx +=1
-        #unicode_string = str(hex(glyph.unicode))[2:]
         
         # altuni, cause glyph expand stroke more times.
         if glyph.changed:
@@ -65,7 +25,6 @@ def simplify(args):
 
         if unicode_int <= 0:
             continue
-        
 
         '''
 glyph.simplify([error_bound, flags, tan_bounds, linefixup, linelenmax])
@@ -101,13 +60,71 @@ If the contour contains just one point then remove it
         glyph.simplify()
         convert_count += 1
 
-
         if convert_count % 100 == 0:
             print("Processing index:%d (count:%d)" % (idx,convert_count))
-
+    
     print("Done, total:%d, convert count:%d" % (idx, convert_count))
-    myfont.save(output_ff)
 
+    return myfont
+
+def simplify(args):
+    ff_path = args.input
+    out_path = args.output
+    
+    # basic settings.
+    ff_path = args.input
+
+    if len(ff_path) > 0:
+        if ff_path[:1] == "/":
+            ff_path = ff_path[:-1]
+
+    if ff_path.endswith(".ttf"):
+        font_name = (basename(normpath(ff_path)))
+        project_path = font_name + ".sfdir"
+        if out_path is None:
+            out_path = project_path
+
+    export_as_font = False
+
+    if out_path is None:
+        if ff_path.endswith(".ttf") or ff_path.endswith(".woff") or ff_path.endswith(".woff2"):
+            export_as_font = True
+    else:
+        if out_path.endswith(".ttf") or out_path.endswith(".woff") or out_path.endswith(".woff2"):
+            export_as_font = True
+
+    if not out_path is None:
+        if out_path.endswith(".sfdir"):
+            if not exists(out_path):
+                mkdir(out_path)
+
+    print("Open font:", ff_path)
+    print("Save dir:", out_path)
+
+    myfont=fontforge.open(ff_path)
+    myfont.selection.all()
+
+    # font config.
+    if not args.comment is None:
+        myfont.comment = args.comment
+    if not args.familyname is None:
+        myfont.familyname = args.familyname
+    if not args.fontname is None:
+        myfont.fontname = args.fontname
+    if not args.fullname is None:
+        myfont.fullname = args.fullname
+    if not args.macstyle is None:
+        myfont.macstyle = args.macstyle
+
+    myfont.simplify(args.error_bound, ('mergelines','ignoreslopes','setstarttoextremum'))
+
+    if export_as_font:
+        myfont.generate(out_path)
+    else:
+        if out_path is None:
+            myfont.save()
+        else:
+            myfont.save(out_path)
 
 def cli():
     parser = argparse.ArgumentParser(
@@ -119,6 +136,7 @@ def cli():
         type=str)
 
     parser.add_argument("--output",
+        default="output.ttf",
         help="output sfdir path",
         type=str)
 
@@ -143,9 +161,9 @@ def cli():
         help="Bold (if set to 1)",
         type=int)
 
+    parser.add_argument('--error_bound', type=float, default=0.1)
 
     args = parser.parse_args()
-
     simplify(args)
 
 
